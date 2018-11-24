43250af0014360c8cdad2eaea24144a171eb4def
RussKie <russkie@gmail.com>
2018-11-24 23:48:47 +1100

    Align assembly information across the project

--- Git.hub/Properties/AssemblyInfo.cs
+++ Git.hub/Properties/AssemblyInfo.cs
@@ -5,31 +5,10 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("Git.hub")]
-[assembly: AssemblyDescription("GitHub API for C#")]
+[assembly: AssemblyDescription("This is a GitExtensions fork of GitHub API for C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GitExtensions")]
 [assembly: AssemblyCopyright("Copyright (c) 2012 Marcus Bauer <mabako@gmail.com>")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
-[assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("008c5a5b-d628-418a-b392-46c56bacde4b")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
