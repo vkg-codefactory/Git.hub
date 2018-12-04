7df924a6a20fb608ff38ffbf95d41b2c9e665a1c
RussKie <russkie@gmail.com>
2018-12-04 23:12:34 +1100

    Fix assembly info

--- Git.hub/Properties/AssemblyInfo.cs
+++ Git.hub/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
@@ -12,3 +13,26 @@ using System.Runtime.InteropServices;
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("008c5a5b-d628-418a-b392-46c56bacde4b")]
+
+[assembly: AssemblyProduct("GitExtensions")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.99.90")]
+[assembly: AssemblyFileVersion("2.99.90")]
+[assembly: AssemblyInformationalVersion("3.00.rc2")]
+
+// Disable CLS compliance. See https://github.com/gitextensions/gitextensions/issues/4710
+[assembly: CLSCompliant(isCompliant: false)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
